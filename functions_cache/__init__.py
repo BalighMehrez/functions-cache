5e19f55958bb953a0b1f81db7bc455bfd3370a1b
Baligh Hatem <baligh.mehrez@nana.sa>
2021-03-19 00:19:54 +0200

    add test cache

--- functions_cache/__init__.py
+++ functions_cache/__init__.py
@@ -3,29 +3,32 @@ import threading
 from threading import Thread
 import functions_cache.engines as engines
 from functions_cache.function_identifier import FunctionIdentifier
-global my_cache
-my_cache = engines.create_engine('sqlite','functions-cache',{})
+_default_engine = engines.create_engine('sqlite','functions-cache',{})
 threadLimiter = threading.BoundedSemaphore(4)
-# def config_engine():
-#     my_cache = engines.create_engine()
 
-def cache_it(_func=None, *, as_daemon=True, is_static=False):
+def config_engine(engine):
+    global _default_engine
+    _default_engine = engine
+
+def cache_it(_func=None, *, as_daemon=True, is_static=False, custom_engine=None):
+    global _default_engine
+    custom_engine = _default_engine if custom_engine is None else custom_engine
 
     def decorator_cache_it(func):
 
         @functools.wraps(func)
         def wrapper_cache_it(*args, **kwargs):
             identifier = FunctionIdentifier(func.__name__,args,kwargs)
-            key = my_cache.create_key(identifier)
-            if my_cache.has_key(key):
-                result = my_cache.get_response_and_time(key)[0]
+            key = custom_engine.create_key(identifier)
+            if custom_engine.has_key(key):
+                result = custom_engine.get_response_and_time(key)[0]
 
                 if not is_static:
 
                     def threaded_func():
                         threadLimiter.acquire()
                         try:
-                            my_cache.save_response(key,func(*args, **kwargs))
+                            custom_engine.save_response(key,func(*args, **kwargs))
                         finally:
                             threadLimiter.release()
                         
@@ -35,7 +38,7 @@ def cache_it(_func=None, *, as_daemon=True, is_static=False):
 
             else:
                 result = func(*args, **kwargs)
-                my_cache.save_response(key,result)
+                custom_engine.save_response(key,result)
             return result
         return wrapper_cache_it
 
