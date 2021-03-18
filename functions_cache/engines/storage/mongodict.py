d1f4491030061b775df61957c25e26457ec9ca47
Baligh Hatem <baligh.mehrez@nana.sa>
2021-03-18 17:26:15 +0200

    add tests

--- functions_cache/engines/storage/mongodict.py
+++ functions_cache/engines/storage/mongodict.py
@@ -50,7 +50,7 @@ class MongoDict(MutableMapping):
             raise KeyError
 
     def __len__(self):
-        return self.collection.count()
+        return self.collection.estimated_document_count()
 
     def __iter__(self):
         for d in self.collection.find({}, {'_id': True}):
